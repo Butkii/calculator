f48b84a7b6cc0b3a529a0e22fd67b9bede86e6d0
Bhakti Ghaghda <72260135+Butkii@users.noreply.github.com>
2022-07-11 11:50:55 +0400

    Created files, testing script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+    return a - b;
+};
+
+const multiply = function(a, b) {
+    return a * b;
+};
+
+const divide = function(a, b) {
+    return a / b;
+};
+
+console.log('2 + 3 = ' + add(2,3));
+console.log('2 - 3 = ' + subtract(2,3));
+console.log('2 * 3 = ' + multiply(2,3));
+console.log('2 / 3 = ' + divide(2,3));
